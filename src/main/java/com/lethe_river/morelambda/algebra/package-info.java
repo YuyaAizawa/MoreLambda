227c8602d6821b4aca8e9aa3bda180092076b47f
YuyaAizawa <selfish.curiosity@gmail.com>
2016-12-20 18:02:51 +0900

    Add package-info

---
+++ src/main/java/com/lethe_river/morelambda/algebra/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 複数の型のデータをまとめるための代数的なコンテナクラスが含まれる.
+ */
+package com.lethe_river.morelambda.algebra;
---
+++ src/main/java/com/lethe_river/morelambda/throwable/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 検査例外が発生する可能性のある関数と操作が含まれる.
+ */
+package com.lethe_river.morelambda.throwable;
