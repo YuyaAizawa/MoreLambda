095e14f2f8b6fb45cf77ddca7a74e119b3d16c48
YuyaAizawa <yaizawa@sa.cs.titech.ac.jp>
2018-10-21 09:52:55 +0900

    Modify, algebra classes to Serializable close #4

--- src/main/java/com/lethe_river/morelambda/algebra/Union3.java
+++ src/main/java/com/lethe_river/morelambda/algebra/Union3.java
@@ -7,196 +7,193 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
- * 型パラメータで指定した，いずれかの型の要素を持つコンテナ・オブジェクト．
- * T1, T2, T3のいずれかの型の要素を持つオブジェクトを定義し，操作を提供する．
- * ユーザーはこのオブジェクトに対し，要素の可能性のある型それぞれに対する操作を記述することで，複数の型を統一的に扱うことができる．
- * 型パラメータT1, T2, T3には異なる型が指定されることを想定している．
- * 
+ * 型パラメータで指定した，いずれかの型の要素を持つコンテナ・オブジェクト． T1, T2,
+ * T3のいずれかの型の要素を持つオブジェクトを定義し，操作を提供する．
+ * ユーザーはこのオブジェクトに対し，要素の可能性のある型それぞれに対する操作を記述することで，複数の型を統一的に扱うことができる． 型パラメータT1,
+ * T2, T3には異なる型が指定されることを想定している．
+ *
  * @author YuyaAizawa
  *
  * @param <T1>
  * @param <T2>
  * @param <T3>
- * 
+ *
  */
 
 public class Union3<T1, T2, T3> implements Serializable {
 	private static final long serialVersionUID = -8438543581530795088L;
 
-	private interface Member<T1, T2, T3> {
+	private interface Member<T1, T2, T3> extends Serializable {
 		public <R> R match(
 				Function<? super T1, ? extends R> f1,
 				Function<? super T2, ? extends R> f2,
 				Function<? super T3, ? extends R> f3);
+
 		public void matchDo(
 				Consumer<? super T1> c1,
 				Consumer<? super T2> c2,
 				Consumer<? super T3> c3);
+
 		public Object getValue();
 	}
-	
+
 	private final Member<T1, T2, T3> member;
 
-	
 	/**
 	 * 指定されたT1型の要素を持つ新しいUnion3を返す．
-	 * 
+	 *
 	 * @param value
 	 * @return 新しいUnion3
 	 * @exception NullPointerException valueがnullの場合
 	 */
 	public static <T1, T2, T3> Union3<T1, T2, T3> of1(T1 value) {
-		return new Union3<T1, T2, T3>(new Member1<>(Objects.requireNonNull(value)));
+		return new Union3<>(new Member1<>(Objects.requireNonNull(value)));
 	}
-	
+
 	/**
 	 * 指定されたT1型の要素を持つ新しいUnion3を返す．
-	 * 
+	 *
 	 * @deprecated {@link Union2#of1(Object) Union3#of1(T1)}に置き換えられた
-	 * 
+	 *
 	 * @param value
 	 * @return 新しいUnion3
 	 * @exception NullPointerException valueがnullの場合
 	 */
+	@Deprecated
 	public static <T1, T2, T3> Union3<T1, T2, T3> _1(T1 value) {
 		return of1(value);
 	}
-	
+
 	/**
 	 * 指定されたT2型の要素を持つ新しいUnion3を返す．
-	 * 
+	 *
 	 * @param value
 	 * @return 新しいUnion3
 	 * @exception NullPointerException valueがnullの場合
 	 */
 	public static <T1, T2, T3> Union3<T1, T2, T3> of2(T2 value) {
-		return new Union3<T1, T2, T3>(new Member2<>(Objects.requireNonNull(value)));
+		return new Union3<>(new Member2<>(Objects.requireNonNull(value)));
 	}
-	
+
 	/**
 	 * 指定されたT2型の要素を持つ新しいUnion3を返す．
-	 * 
+	 *
 	 * @deprecated {@link Union2#of1(Object) Union3#of2(T2)}に置き換えられた
-	 * 
+	 *
 	 * @param value
 	 * @return 新しいUnion3
 	 * @exception NullPointerException valueがnullの場合
 	 */
+	@Deprecated
 	public static <T1, T2, T3> Union3<T1, T2, T3> _2(T2 value) {
 		return of2(value);
 	}
-	
+
 	/**
 	 * 指定されたT3型の要素を持つ新しいUnion3を返す．
-	 * 
+	 *
 	 * @param value
 	 * @return 新しいUnion3
 	 * @exception NullPointerException valueがnullの場合
 	 */
 	public static <T1, T2, T3> Union3<T1, T2, T3> of3(T3 value) {
-		return new Union3<T1, T2, T3>(new Member3<>(Objects.requireNonNull(value)));
+		return new Union3<>(new Member3<>(Objects.requireNonNull(value)));
 	}
-	
+
 	/**
 	 * 指定されたT3型の要素を持つ新しいUnion3を返す．
-	 * 
+	 *
 	 * @deprecated {@link Union2#of1(Object) Union3#of3(T3)}に置き換えられた
-	 * 
+	 *
 	 * @param value
 	 * @return 新しいUnion3
 	 * @exception NullPointerException valueがnullの場合
 	 */
+	@Deprecated
 	public static <T1, T2, T3> Union3<T1, T2, T3> _3(T3 value) {
 		return of3(value);
 	}
-	
+
 	/**
-	 * 要素に関数を適用し，結果を返す．
-	 * 与えたそれぞれの関数のうち，要素の型に対する関数が適用され，結果が返る．
+	 * 要素に関数を適用し，結果を返す． 与えたそれぞれの関数のうち，要素の型に対する関数が適用され，結果が返る．
 	 * 与える関数の戻り値の型は一致していなければならない．
-	 * 
+	 *
 	 * @param f1 T1に適用する関数
 	 * @param f2 T2に適用する関数
 	 * @param f3 T3に適用する関数
 	 * @return 関数の戻り値
-	*/
+	 */
 	public final <R> R match(
-				Function<? super T1, ? extends R> f1,
-				Function<? super T2, ? extends R> f2,
-				Function<? super T3, ? extends R> f3) {
+			Function<? super T1, ? extends R> f1,
+			Function<? super T2, ? extends R> f2,
+			Function<? super T3, ? extends R> f3) {
 		return member.match(f1, f2, f3);
 	}
-	
+
 	/**
-	 * 要素に対してオペレーションを実行する．
-	 * 与えたそれぞれのオペレーションのうち，要素の型に対するオペレーションが実行される．
-	 * 
+	 * 要素に対してオペレーションを実行する． 与えたそれぞれのオペレーションのうち，要素の型に対するオペレーションが実行される．
+	 *
 	 * @param c1 T1に対するオペレーション
 	 * @param c2 T2に対するオペレーション
 	 * @param c3 T3に対するオペレーション
 	 */
 	public final void matchDo(
-				Consumer<? super T1> c1,
-				Consumer<? super T2> c2,
-				Consumer<? super T3> c3) {
+			Consumer<? super T1> c1,
+			Consumer<? super T2> c2,
+			Consumer<? super T3> c3) {
 		member.matchDo(c1, c2, c3);
 	}
 
-	
 	/**
-	 * このインスタンスのT1型の要素を返す．
-	 * このインスタンスのT1型の要素を表すOptionalを返す．要素がT1型でない場合は空のOptionalを返す．
-	 * 
+	 * このインスタンスのT1型の要素を返す． このインスタンスのT1型の要素を表すOptionalを返す．要素がT1型でない場合は空のOptionalを返す．
+	 *
 	 * @return T1型の要素を表すOptional,または空のOptional
 	 */
 	public Optional<T1> get1() {
 		return Optional.ofNullable(match(t1 -> t1, t2 -> null, t3 -> null));
 	}
-	
+
 	/**
-	 * このインスタンスのT2型の要素を返す．
-	 * このインスタンスのT2型の要素を表すOptionalを返す．要素がT2型でない場合は空のOptionalを返す．
-	 * 
+	 * このインスタンスのT2型の要素を返す． このインスタンスのT2型の要素を表すOptionalを返す．要素がT2型でない場合は空のOptionalを返す．
+	 *
 	 * @return T2型の要素を表すOptional,または空のOptional
 	 */
 	public Optional<T2> get2() {
 		return Optional.ofNullable(match(t1 -> null, t2 -> t2, t3 -> null));
 	}
-	
+
 	/**
-	 * このインスタンスのT3型の要素を返す．
-	 * このインスタンスのT3型の要素を表すOptionalを返す．要素がT3型でない場合は空のOptionalを返す．
-	 * 
+	 * このインスタンスのT3型の要素を返す． このインスタンスのT3型の要素を表すOptionalを返す．要素がT3型でない場合は空のOptionalを返す．
+	 *
 	 * @return T3型の要素を表すOptional,または空のOptional
 	 */
 	public Optional<T3> get3() {
 		return Optional.ofNullable(match(t1 -> null, t2 -> null, t3 -> t3));
 	}
-	
+
 	/**
 	 * 要素の文字列表現を返す.
-	 * 
+	 *
 	 * @return 要素の文字列表現
 	 */
 	@Override
 	public String toString() {
 		return match(T1::toString, T2::toString, T3::toString);
 	}
-	
+
 	/**
 	 * 要素に基づくハッシュを返す．
-	 * 
+	 *
 	 * @return 要素に基づくハッシュ
 	 */
 	@Override
 	public int hashCode() {
 		return member.getValue().hashCode();
 	}
-	
+
 	/**
-	 * 指定されたオブジェクトがUnion3と等しいか比較する．
-	 * 指定されたオブジェクトもUnion3であり，内部の要素が等しいときにtrueをかえす．
-	 * 
+	 * 指定されたオブジェクトがUnion3と等しいか比較する． 指定されたオブジェクトもUnion3であり，内部の要素が等しいときにtrueをかえす．
+	 *
 	 * @return 指定されたオブジェクトがUnionと等しい場合は true
 	 */
 	@Override
@@ -208,7 +205,7 @@ public class Union3<T1, T2, T3> implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		@SuppressWarnings("rawtypes")
-		Object other = ((Union3)obj).member.getValue();
+		Object other = ((Union3) obj).member.getValue();
 		return member.getValue().equals(other);
 	}
 
@@ -217,8 +214,7 @@ public class Union3<T1, T2, T3> implements Serializable {
 	}
 
 	/**
-	 * 指定されたUnion3を用いてインスタンスを初期化する．
-	 * このクラスを継承したクラスを作るときに利用する．
+	 * 指定されたUnion3を用いてインスタンスを初期化する． このクラスを継承したクラスを作るときに利用する．
 	 *
 	 * @param base 初期化に用いるインスタンス
 	 */
@@ -226,15 +222,14 @@ public class Union3<T1, T2, T3> implements Serializable {
 		this.member = base.member;
 	}
 
-	
 	private static class Member1<T1, T2, T3> implements Member<T1, T2, T3> {
-		
+		private static final long serialVersionUID = -3271772556698249315L;
 		private final T1 value;
-		
+
 		Member1(T1 value) {
 			this.value = value;
 		}
-		
+
 		@Override
 		public <R> R match(
 				Function<? super T1, ? extends R> f1,
@@ -242,7 +237,7 @@ public class Union3<T1, T2, T3> implements Serializable {
 				Function<? super T3, ? extends R> f3) {
 			return f1.apply(value);
 		}
-		
+
 		@Override
 		public void matchDo(
 				Consumer<? super T1> c1,
@@ -250,21 +245,21 @@ public class Union3<T1, T2, T3> implements Serializable {
 				Consumer<? super T3> c3) {
 			c1.accept(value);
 		}
-		
+
 		@Override
 		public Object getValue() {
 			return value;
 		}
 	}
-	
+
 	private static class Member2<T1, T2, T3> implements Member<T1, T2, T3> {
-		
+		private static final long serialVersionUID = -7134409932751326359L;
 		private final T2 value;
-		
+
 		Member2(T2 value) {
 			this.value = value;
 		}
-		
+
 		@Override
 		public <R> R match(
 				Function<? super T1, ? extends R> f1,
@@ -272,7 +267,7 @@ public class Union3<T1, T2, T3> implements Serializable {
 				Function<? super T3, ? extends R> f3) {
 			return f2.apply(value);
 		}
-		
+
 		@Override
 		public void matchDo(
 				Consumer<? super T1> c1,
@@ -280,21 +275,21 @@ public class Union3<T1, T2, T3> implements Serializable {
 				Consumer<? super T3> c3) {
 			c2.accept(value);
 		}
-		
+
 		@Override
 		public Object getValue() {
 			return value;
 		}
 	}
-	
+
 	private static class Member3<T1, T2, T3> implements Member<T1, T2, T3> {
-		
+		private static final long serialVersionUID = -2620602697424978522L;
 		private final T3 value;
-		
+
 		Member3(T3 value) {
 			this.value = value;
 		}
-		
+
 		@Override
 		public <R> R match(
 				Function<? super T1, ? extends R> f1,
@@ -302,7 +297,7 @@ public class Union3<T1, T2, T3> implements Serializable {
 				Function<? super T3, ? extends R> f3) {
 			return f3.apply(value);
 		}
-		
+
 		@Override
 		public void matchDo(
 				Consumer<? super T1> c1,
@@ -310,7 +305,7 @@ public class Union3<T1, T2, T3> implements Serializable {
 				Consumer<? super T3> c3) {
 			c3.accept(value);
 		}
-		
+
 		@Override
 		public Object getValue() {
 			return value;
--- src/main/java/com/lethe_river/morelambda/algebra/Union4.java
+++ src/main/java/com/lethe_river/morelambda/algebra/Union4.java
@@ -7,237 +7,234 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
- * 型パラメータで指定した，いずれかの型の要素を持つコンテナ・オブジェクト．
- * T1, T2, T3, T4のいずれかの型の要素を持つオブジェクトを定義し，操作を提供する．
- * ユーザーはこのオブジェクトに対し，要素の可能性のある型それぞれに対する操作を記述することで，複数の型を統一的に扱うことができる．
- * 型パラメータT1, T2, T3, T4には異なる型が指定されることを想定している．
- * 
+ * 型パラメータで指定した，いずれかの型の要素を持つコンテナ・オブジェクト． T1, T2, T3,
+ * T4のいずれかの型の要素を持つオブジェクトを定義し，操作を提供する．
+ * ユーザーはこのオブジェクトに対し，要素の可能性のある型それぞれに対する操作を記述することで，複数の型を統一的に扱うことができる． 型パラメータT1,
+ * T2, T3, T4には異なる型が指定されることを想定している．
+ *
  * @author YuyaAizawa
  *
  * @param <T1>
  * @param <T2>
  * @param <T3>
  * @param <T4>
- * 
+ *
  */
 
 public class Union4<T1, T2, T3, T4> implements Serializable {
 	private static final long serialVersionUID = 2634099534334851651L;
 
-	private interface Member<T1, T2, T3, T4> {
+	private interface Member<T1, T2, T3, T4> extends Serializable {
 		public <R> R match(
 				Function<? super T1, ? extends R> f1,
 				Function<? super T2, ? extends R> f2,
 				Function<? super T3, ? extends R> f3,
 				Function<? super T4, ? extends R> f4);
+
 		public void matchDo(
 				Consumer<? super T1> c1,
 				Consumer<? super T2> c2,
 				Consumer<? super T3> c3,
 				Consumer<? super T4> c4);
+
 		public Object getValue();
 	}
-	
+
 	private final Member<T1, T2, T3, T4> member;
 
-	
 	/**
 	 * 指定されたT1型の要素を持つ新しいUnion4を返す．
-	 * 
+	 *
 	 * @param value
 	 * @return 新しいUnion4
 	 * @exception NullPointerException valueがnullの場合
 	 */
 	public static <T1, T2, T3, T4> Union4<T1, T2, T3, T4> of1(T1 value) {
-		return new Union4<T1, T2, T3, T4>(new Member1<>(Objects.requireNonNull(value)));
+		return new Union4<>(new Member1<>(Objects.requireNonNull(value)));
 	}
-	
+
 	/**
 	 * 指定されたT1型の要素を持つ新しいUnion4を返す．
-	 * 
+	 *
 	 * @deprecated {@link Union2#of1(Object) Union4#of1(T1)}に置き換えられた
-	 * 
+	 *
 	 * @param value
 	 * @return 新しいUnion4
 	 * @exception NullPointerException valueがnullの場合
 	 */
+	@Deprecated
 	public static <T1, T2, T3, T4> Union4<T1, T2, T3, T4> _1(T1 value) {
 		return of1(value);
 	}
-	
+
 	/**
 	 * 指定されたT2型の要素を持つ新しいUnion4を返す．
-	 * 
+	 *
 	 * @param value
 	 * @return 新しいUnion4
 	 * @exception NullPointerException valueがnullの場合
 	 */
 	public static <T1, T2, T3, T4> Union4<T1, T2, T3, T4> of2(T2 value) {
-		return new Union4<T1, T2, T3, T4>(new Member2<>(Objects.requireNonNull(value)));
+		return new Union4<>(new Member2<>(Objects.requireNonNull(value)));
 	}
-	
+
 	/**
 	 * 指定されたT2型の要素を持つ新しいUnion4を返す．
-	 * 
+	 *
 	 * @deprecated {@link Union2#of1(Object) Union4#of2(T2)}に置き換えられた
-	 * 
+	 *
 	 * @param value
 	 * @return 新しいUnion4
 	 * @exception NullPointerException valueがnullの場合
 	 */
+	@Deprecated
 	public static <T1, T2, T3, T4> Union4<T1, T2, T3, T4> _2(T2 value) {
 		return of2(value);
 	}
-	
+
 	/**
 	 * 指定されたT3型の要素を持つ新しいUnion4を返す．
-	 * 
+	 *
 	 * @param value
 	 * @return 新しいUnion4
 	 * @exception NullPointerException valueがnullの場合
 	 */
 	public static <T1, T2, T3, T4> Union4<T1, T2, T3, T4> of3(T3 value) {
-		return new Union4<T1, T2, T3, T4>(new Member3<>(Objects.requireNonNull(value)));
+		return new Union4<>(new Member3<>(Objects.requireNonNull(value)));
 	}
-	
+
 	/**
 	 * 指定されたT3型の要素を持つ新しいUnion4を返す．
-	 * 
+	 *
 	 * @deprecated {@link Union2#of1(Object) Union4#of3(T3)}に置き換えられた
-	 * 
+	 *
 	 * @param value
 	 * @return 新しいUnion4
 	 * @exception NullPointerException valueがnullの場合
 	 */
+	@Deprecated
 	public static <T1, T2, T3, T4> Union4<T1, T2, T3, T4> _3(T3 value) {
 		return of3(value);
 	}
-	
+
 	/**
 	 * 指定されたT4型の要素を持つ新しいUnion4を返す．
-	 * 
+	 *
 	 * @param value
 	 * @return 新しいUnion4
 	 * @exception NullPointerException valueがnullの場合
 	 */
 	public static <T1, T2, T3, T4> Union4<T1, T2, T3, T4> of4(T4 value) {
-		return new Union4<T1, T2, T3, T4>(new Member4<>(Objects.requireNonNull(value)));
+		return new Union4<>(new Member4<>(Objects.requireNonNull(value)));
 	}
-	
+
 	/**
 	 * 指定されたT4型の要素を持つ新しいUnion4を返す．
-	 * 
+	 *
 	 * @deprecated {@link Union2#of1(Object) Union4#of4(T4)}に置き換えられた
-	 * 
+	 *
 	 * @param value
 	 * @return 新しいUnion4
 	 * @exception NullPointerException valueがnullの場合
 	 */
+	@Deprecated
 	public static <T1, T2, T3, T4> Union4<T1, T2, T3, T4> _4(T4 value) {
 		return of4(value);
 	}
-	
+
 	/**
-	 * 要素に関数を適用し，結果を返す．
-	 * 与えたそれぞれの関数のうち，要素の型に対する関数が適用され，結果が返る．
+	 * 要素に関数を適用し，結果を返す． 与えたそれぞれの関数のうち，要素の型に対する関数が適用され，結果が返る．
 	 * 与える関数の戻り値の型は一致していなければならない．
-	 * 
+	 *
 	 * @param f1 T1に適用する関数
 	 * @param f2 T2に適用する関数
 	 * @param f3 T3に適用する関数
 	 * @param f4 T4に適用する関数
 	 * @return 関数の戻り値
-	*/
+	 */
 	public final <R> R match(
-				Function<? super T1, ? extends R> f1,
-				Function<? super T2, ? extends R> f2,
-				Function<? super T3, ? extends R> f3,
-				Function<? super T4, ? extends R> f4) {
+			Function<? super T1, ? extends R> f1,
+			Function<? super T2, ? extends R> f2,
+			Function<? super T3, ? extends R> f3,
+			Function<? super T4, ? extends R> f4) {
 		return member.match(f1, f2, f3, f4);
 	}
-	
+
 	/**
-	 * 要素に対してオペレーションを実行する．
-	 * 与えたそれぞれのオペレーションのうち，要素の型に対するオペレーションが実行される．
-	 * 
+	 * 要素に対してオペレーションを実行する． 与えたそれぞれのオペレーションのうち，要素の型に対するオペレーションが実行される．
+	 *
 	 * @param c1 T1に対するオペレーション
 	 * @param c2 T2に対するオペレーション
 	 * @param c3 T3に対するオペレーション
 	 * @param c4 T4に対するオペレーション
 	 */
 	public final void matchDo(
-				Consumer<? super T1> c1,
-				Consumer<? super T2> c2,
-				Consumer<? super T3> c3,
-				Consumer<? super T4> c4) {
+			Consumer<? super T1> c1,
+			Consumer<? super T2> c2,
+			Consumer<? super T3> c3,
+			Consumer<? super T4> c4) {
 		member.matchDo(c1, c2, c3, c4);
 	}
 
-	
 	/**
-	 * このインスタンスのT1型の要素を返す．
-	 * このインスタンスのT1型の要素を表すOptionalを返す．要素がT1型でない場合は空のOptionalを返す．
-	 * 
+	 * このインスタンスのT1型の要素を返す． このインスタンスのT1型の要素を表すOptionalを返す．要素がT1型でない場合は空のOptionalを返す．
+	 *
 	 * @return T1型の要素を表すOptional,または空のOptional
 	 */
 	public Optional<T1> get1() {
 		return Optional.ofNullable(match(t1 -> t1, t2 -> null, t3 -> null, t4 -> null));
 	}
-	
+
 	/**
-	 * このインスタンスのT2型の要素を返す．
-	 * このインスタンスのT2型の要素を表すOptionalを返す．要素がT2型でない場合は空のOptionalを返す．
-	 * 
+	 * このインスタンスのT2型の要素を返す． このインスタンスのT2型の要素を表すOptionalを返す．要素がT2型でない場合は空のOptionalを返す．
+	 *
 	 * @return T2型の要素を表すOptional,または空のOptional
 	 */
 	public Optional<T2> get2() {
 		return Optional.ofNullable(match(t1 -> null, t2 -> t2, t3 -> null, t4 -> null));
 	}
-	
+
 	/**
-	 * このインスタンスのT3型の要素を返す．
-	 * このインスタンスのT3型の要素を表すOptionalを返す．要素がT3型でない場合は空のOptionalを返す．
-	 * 
+	 * このインスタンスのT3型の要素を返す． このインスタンスのT3型の要素を表すOptionalを返す．要素がT3型でない場合は空のOptionalを返す．
+	 *
 	 * @return T3型の要素を表すOptional,または空のOptional
 	 */
 	public Optional<T3> get3() {
 		return Optional.ofNullable(match(t1 -> null, t2 -> null, t3 -> t3, t4 -> null));
 	}
-	
+
 	/**
-	 * このインスタンスのT4型の要素を返す．
-	 * このインスタンスのT4型の要素を表すOptionalを返す．要素がT4型でない場合は空のOptionalを返す．
-	 * 
+	 * このインスタンスのT4型の要素を返す． このインスタンスのT4型の要素を表すOptionalを返す．要素がT4型でない場合は空のOptionalを返す．
+	 *
 	 * @return T4型の要素を表すOptional,または空のOptional
 	 */
 	public Optional<T4> get4() {
 		return Optional.ofNullable(match(t1 -> null, t2 -> null, t3 -> null, t4 -> t4));
 	}
-	
+
 	/**
 	 * 要素の文字列表現を返す.
-	 * 
+	 *
 	 * @return 要素の文字列表現
 	 */
 	@Override
 	public String toString() {
 		return match(T1::toString, T2::toString, T3::toString, T4::toString);
 	}
-	
+
 	/**
 	 * 要素に基づくハッシュを返す．
-	 * 
+	 *
 	 * @return 要素に基づくハッシュ
 	 */
 	@Override
 	public int hashCode() {
 		return member.getValue().hashCode();
 	}
-	
+
 	/**
-	 * 指定されたオブジェクトがUnion4と等しいか比較する．
-	 * 指定されたオブジェクトもUnion4であり，内部の要素が等しいときにtrueをかえす．
-	 * 
+	 * 指定されたオブジェクトがUnion4と等しいか比較する． 指定されたオブジェクトもUnion4であり，内部の要素が等しいときにtrueをかえす．
+	 *
 	 * @return 指定されたオブジェクトがUnionと等しい場合は true
 	 */
 	@Override
@@ -249,7 +246,7 @@ public class Union4<T1, T2, T3, T4> implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		@SuppressWarnings("rawtypes")
-		Object other = ((Union4)obj).member.getValue();
+		Object other = ((Union4) obj).member.getValue();
 		return member.getValue().equals(other);
 	}
 
@@ -258,8 +255,7 @@ public class Union4<T1, T2, T3, T4> implements Serializable {
 	}
 
 	/**
-	 * 指定されたUnion4を用いてインスタンスを初期化する．
-	 * このクラスを継承したクラスを作るときに利用する．
+	 * 指定されたUnion4を用いてインスタンスを初期化する． このクラスを継承したクラスを作るときに利用する．
 	 *
 	 * @param base 初期化に用いるインスタンス
 	 */
@@ -267,15 +263,14 @@ public class Union4<T1, T2, T3, T4> implements Serializable {
 		this.member = base.member;
 	}
 
-	
 	private static class Member1<T1, T2, T3, T4> implements Member<T1, T2, T3, T4> {
-		
+		private static final long serialVersionUID = 5447555391854330314L;
 		private final T1 value;
-		
+
 		Member1(T1 value) {
 			this.value = value;
 		}
-		
+
 		@Override
 		public <R> R match(
 				Function<? super T1, ? extends R> f1,
@@ -284,7 +279,7 @@ public class Union4<T1, T2, T3, T4> implements Serializable {
 				Function<? super T4, ? extends R> f4) {
 			return f1.apply(value);
 		}
-		
+
 		@Override
 		public void matchDo(
 				Consumer<? super T1> c1,
@@ -293,21 +288,21 @@ public class Union4<T1, T2, T3, T4> implements Serializable {
 				Consumer<? super T4> c4) {
 			c1.accept(value);
 		}
-		
+
 		@Override
 		public Object getValue() {
 			return value;
 		}
 	}
-	
+
 	private static class Member2<T1, T2, T3, T4> implements Member<T1, T2, T3, T4> {
-		
+		private static final long serialVersionUID = 645836686681736450L;
 		private final T2 value;
-		
+
 		Member2(T2 value) {
 			this.value = value;
 		}
-		
+
 		@Override
 		public <R> R match(
 				Function<? super T1, ? extends R> f1,
@@ -316,7 +311,7 @@ public class Union4<T1, T2, T3, T4> implements Serializable {
 				Function<? super T4, ? extends R> f4) {
 			return f2.apply(value);
 		}
-		
+
 		@Override
 		public void matchDo(
 				Consumer<? super T1> c1,
@@ -325,21 +320,21 @@ public class Union4<T1, T2, T3, T4> implements Serializable {
 				Consumer<? super T4> c4) {
 			c2.accept(value);
 		}
-		
+
 		@Override
 		public Object getValue() {
 			return value;
 		}
 	}
-	
+
 	private static class Member3<T1, T2, T3, T4> implements Member<T1, T2, T3, T4> {
-		
+		private static final long serialVersionUID = -788215284503725758L;
 		private final T3 value;
-		
+
 		Member3(T3 value) {
 			this.value = value;
 		}
-		
+
 		@Override
 		public <R> R match(
 				Function<? super T1, ? extends R> f1,
@@ -348,7 +343,7 @@ public class Union4<T1, T2, T3, T4> implements Serializable {
 				Function<? super T4, ? extends R> f4) {
 			return f3.apply(value);
 		}
-		
+
 		@Override
 		public void matchDo(
 				Consumer<? super T1> c1,
@@ -357,21 +352,21 @@ public class Union4<T1, T2, T3, T4> implements Serializable {
 				Consumer<? super T4> c4) {
 			c3.accept(value);
 		}
-		
+
 		@Override
 		public Object getValue() {
 			return value;
 		}
 	}
-	
+
 	private static class Member4<T1, T2, T3, T4> implements Member<T1, T2, T3, T4> {
-		
+		private static final long serialVersionUID = -6910462512745226674L;
 		private final T4 value;
-		
+
 		Member4(T4 value) {
 			this.value = value;
 		}
-		
+
 		@Override
 		public <R> R match(
 				Function<? super T1, ? extends R> f1,
@@ -380,7 +375,7 @@ public class Union4<T1, T2, T3, T4> implements Serializable {
 				Function<? super T4, ? extends R> f4) {
 			return f4.apply(value);
 		}
-		
+
 		@Override
 		public void matchDo(
 				Consumer<? super T1> c1,
@@ -389,7 +384,7 @@ public class Union4<T1, T2, T3, T4> implements Serializable {
 				Consumer<? super T4> c4) {
 			c4.accept(value);
 		}
-		
+
 		@Override
 		public Object getValue() {
 			return value;
